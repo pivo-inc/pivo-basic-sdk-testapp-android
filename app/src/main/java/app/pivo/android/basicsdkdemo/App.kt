f53a17b86ae0dff29850c2a2646a83698bed6f19
murodjon <murodjon@next-aeon.com>
2020-04-17 14:18:01 +0900

    add license reader

--- app/src/main/java/app/pivo/android/basicsdkdemo/App.kt
+++ app/src/main/java/app/pivo/android/basicsdkdemo/App.kt
@@ -2,6 +2,10 @@ package app.pivo.android.basicsdkdemo
 
 import android.app.Application
 import app.pivo.android.basicsdk.PivoSdk
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
 
 /**
  * Created by murodjon on 2020/04/01
@@ -13,5 +17,25 @@ class App: Application()
 
         //initialize PivoSdk
         PivoSdk.init(this)
+        PivoSdk.getInstance().unlockWithLicenseKey(getLicenseContent())
+
+    }
+
+    private fun getLicenseContent():String?{
+        var inputStream = assets.open("licenceKey.json")
+        return readFile(inputStream)
+    }
+
+    @Throws(IOException::class)
+    fun readFile(inputStream: InputStream?): String? {
+        val str = StringBuilder()
+        val br: BufferedReader
+        br = BufferedReader(InputStreamReader(inputStream))
+        var line: String?=null
+        while (br.readLine().also({ line = it }) != null) {
+            str.append(line)
+        }
+        br.close()
+        return str.toString()
     }
 }
